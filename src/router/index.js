cfc422a04112a3d7155a67731e76869ab7124eda
NiRongxu <nrx19940305>
2018-10-29 15:34:29 +0800

    更新不了，提交一点东西

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import CommerViews from '@/views/commerViews.vue'
 import Login from '@/views/login/index.vue'
+import Home from '@/views/Home.vue'
+import HomeMain from '@/views/mainIndex.vue'
+import Icon from '@/views/icon/index'
+import Erji from '@/views/duoji/erji.vue'
+import Erji2 from '@/views/duoji/erji2.vue'
+import Sanji from '@/views/duoji/sanji.vue'
+import Sanji2 from '@/views/duoji/sanji2.vue'
+import Siji from '@/views/duoji/siji.vue'
+import Wuji from '@/views/duoji/wuji.vue'
+import Transfer from '@/views/transfer/transfer.vue'
+import DataTable from '@/views/table/dataTables.vue'
+import FilterTable from '@/views/table/filterTable.vue'
+import DragTabe from '@/views/table/dragTabe.vue'
+import Upload from '@/views/upload/upload.vue'
+import Markdown from '@/views/markdown/markdownView.vue'
 
 Vue.use(Router)
 
@@ -17,6 +33,192 @@ export default new Router({
       name: '',
       hidden: true,
       children: []
+    },
+    {
+      path: '/404',
+      // component: NotFound,
+      name: '',
+      hidden: true,
+      children: []
+    },
+    {
+      path: '/index',
+      iconCls: 'fa fa-dashboard', // 图标样式class
+      name: '首页',
+      component: Home,
+      alone: true,
+      children: [
+        {
+          path: '/index',
+          iconCls: 'fa fa-dashboard', // 图标样式class
+          name: '首页',
+          component: HomeMain,
+          children: []
+        }
+      ]
+    },
+    {
+      path: '/',
+      iconCls: 'fa fa-paw', // 图标样式class
+      name: '图标',
+      component: Home,
+      children: [
+        {
+          path: '/icon',
+          iconCls: 'fa fa-meh-o', // 图标样式class
+          name: '图标',
+          component: Icon,
+          children: []
+        }
+      ]
+    },
+    {
+      path: '/',
+      iconCls: 'fa fa-server', // 图标样式class
+      name: '穿梭框',
+      component: Home,
+      children: [
+        {
+          path: '/transfer',
+          iconCls: 'fa fa-server', // 图标样式class
+          name: '穿梭框',
+          component: Transfer,
+          children: []
+        }
+      ]
+    },
+    {
+      path: '/',
+      iconCls: 'fa fa-server', // 图标样式class
+      name: '表格',
+      component: Home,
+      children: [
+        {
+          path: '/dataTable',
+          iconCls: 'fa fa-server', // 图标样式class
+          name: '多选数据表格',
+          component: DataTable,
+          children: []
+        },
+        {
+          path: '/filterTable',
+          iconCls: 'fa fa-server', // 图标样式class
+          name: '筛选表格',
+          component: FilterTable,
+          children: []
+        },
+        {
+          path: '/dragTabe',
+          iconCls: 'fa fa-server', // 图标样式class
+          name: '拖拽排序',
+          component: DragTabe,
+          children: []
+        }
+      ]
+    },
+    {
+      path: '/',
+      iconCls: 'fa fa-server', // 图标样式class
+      name: '上传',
+      component: Home,
+      children: [
+        {
+          path: '/upload',
+          iconCls: 'fa fa-server', // 图标样式class
+          name: '上传',
+          component: Upload,
+          children: []
+        }
+      ]
+    },
+    {
+      path: '/',
+      iconCls: 'fa fa-server', // 图标样式class
+      name: '编辑器',
+      component: Home,
+      children: [
+        {
+          path: '/markdown',
+          iconCls: 'fa fa-server', // 图标样式class
+          name: 'markdown',
+          component: Markdown,
+          children: []
+        }
+      ]
+    },
+    {
+      path: '/',
+      iconCls: 'fa fa-server',
+      name: '多级目录',
+      component: Home,
+      children: [
+        {
+          path: '/erji',
+          iconCls: 'fa fa-server',
+          name: '二级',
+          component: Erji,
+          children: []
+        },
+        {
+          path: '/erji2',
+          iconCls: 'fa fa-server',
+          name: '二级2',
+          component: Erji2,
+          children: []
+        },
+        {
+          path: '/erji6',
+          iconCls: 'fa fa-server',
+          name: '二级-1',
+          component: CommerViews,
+          children: [
+            {
+              path: '/sanji',
+              iconCls: 'fa fa-server',
+              name: '三级',
+              component: Sanji,
+              children: []
+            },
+            {
+              path: '/sanji1',
+              iconCls: 'fa fa-server',
+              name: '三级1',
+              component: Sanji2,
+              children: []
+            },
+            {
+              path: '/sanji2',
+              iconCls: 'fa fa-server',
+              name: '三级-1',
+              component: CommerViews,
+              children: [
+                {
+                  path: '/siji',
+                  iconCls: 'fa fa-server',
+                  name: '四级',
+                  component: Siji,
+                  children: []
+                },
+                {
+                  path: '/siji1',
+                  iconCls: 'fa fa-server',
+                  name: '四级-1',
+                  component: CommerViews,
+                  children: [
+                    {
+                      path: '/wuji',
+                      iconCls: 'fa fa-server',
+                      name: '五级',
+                      component: Wuji,
+                      children: []
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        }
+      ]
     }
 
   ]
---
+++ src/views/article/addArticle.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="addArticle">
+    <el-form ref="article" :model="article" label-width="80px">
+      <el-form-item label="文章标题">
+        <el-input v-model="article.title"></el-input>
+      </el-form-item>
+      <el-form-item label="标签">
+        <el-autocomplete
+          class="inline-input"
+          v-model="article.tag"
+          :fetch-suggestions="querySearch"
+          placeholder="请输入内容"
+          @select="handleSelect"
+        ></el-autocomplete>
+      </el-form-item>
+      <el-form-item label="是否置顶">
+        <el-switch v-model="article.top"></el-switch>
+      </el-form-item>
+    </el-form>
+    <markdown :onchange="change"></markdown>
+    <el-row type="flex" class="row-bg" justify="end">
+      <el-button class="subBtn" type="primary" @click="submitArticle">发布</el-button>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import Markdown from '../../components/markdown/markdown-editor'
+export default {
+  name: 'addArticle',
+  data () {
+    return {
+      article: {
+        title: '',
+        tag: '',
+        top: false,
+        content: {}
+      },
+      restaurants: []
+    }
+  },
+  components: {Markdown},
+  methods: {
+    change () {
+      console.log(arguments[0], this.article)
+      this.article.content = arguments[0]
+    },
+    querySearch (queryString, cb) {
+      var restaurants = this.restaurants
+      var results = queryString ? restaurants.filter(this.createFilter(queryString)) : restaurants
+      // 调用 callback 返回建议列表的数据
+      cb(results)
+    },
+    createFilter (queryString) {
+      return (restaurant) => {
+        return (restaurant.value.toLowerCase().indexOf(queryString.toLowerCase()) === 0)
+      }
+    },
+    loadAll () {
+      return [
+        { 'value': 'vue' },
+        { 'value': 'node.js' }
+      ]
+    },
+    handleSelect (item) {
+      console.log(item)
+    },
+    submitArticle () {
+      this.$axios.post('/api/addArticle', {
+        data: this.article
+      })
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+  },
+  mounted () {
+    this.restaurants = this.loadAll()
+  }
+}
+</script>
+
+<style scoped>
+.subBtn{
+  width: 100px;
+  margin: 0 auto;
+}
+</style>
